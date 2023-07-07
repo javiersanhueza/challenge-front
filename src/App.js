0093fc258b2ed48253d22dbda1a7161c11aa17eb
Javier  Sanhueza <javier.sanhueza.14@sansano.usm.cl>
2023-07-06 23:07:50 -0400

    organizando espacio de trabajo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="my-5 text-center">
+        <button className="btn btn-primary">Botón bootstrap</button>
+      </div>
     </div>
   );
 }
